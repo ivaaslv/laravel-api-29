e6da2a3c9a3158ec8d1c87f459f7a3d87d6da1f3
silva naura <silvanaurahanifa@gmail.com>
2026-08-03 20:15:20 +0700

    perbaikan nama route

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware(['auth:sanctum'])->get('/user', function)
 // })->middleware('auth:sanctum');
 
-Route::get('/product', [ProductController::class, 'index'])->name('product');
-Route::post('/product', [ProductController::class, 'store'])->name('product');
-Route::put('/product/{product}', [ProductController::class, 'update'])->name('product');
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+Route::put('/product/{product}', [ProductController::class, 'update'])->name('product.update');
